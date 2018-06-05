9048b45fbeeebab498f8a205c77fa5be156124d2
lastsoup <qingtang166@qq.com>
2018-06-05 15:19:43 +0800

    add

--- public/index1.js
+++
@@ -1,6 +0,0 @@
-import './css/app.css'
-import './js/swiper/swiper.min.css'
-import './css/tab.css'
-/*method*/
-import './js/plugin.method.js'
-import './app.js'
--- public/index2.js
+++
@@ -1,3 +0,0 @@
-import './js/plugin.method.js'
-import './app.js'
-import './js/service.js'
